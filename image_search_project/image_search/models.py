60d87ea382175ca23a8f888790a1ce5165929044
Bishal Shrestha <136599301+bigketa10@users.noreply.github.com>
2024-09-04 18:37:20 +0100

    more changes

--- image_search_project/image_search/models.py
+++ image_search_project/image_search/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from .utils import extract_features
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images/')
+    feature_vector = models.JSONField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        # Save the image first to generate the file path
+        super().save(*args, **kwargs)
+        
+        # Extract features from the image
+        features = extract_features(self.image.path)
+        self.feature_vector = features.tolist()  # Convert numpy array to list
+        super().save(*args, **kwargs)  # Save again to store the feature vector
